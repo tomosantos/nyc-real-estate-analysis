43e9de5ca39bc5e2b89eb1139fa415a50537368c
Wellinton Oliveira <wellintonoliveira04@gmail.com>
2024-04-30 11:30:42 -0300

    update

--- app.py
+++ app.py
@@ -5,5 +5,4 @@ import dash_bootstrap_components as dbc
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.SLATE])
 server = app.server
 app.scripts.config.serve_locally = True
-server = app.server
  
